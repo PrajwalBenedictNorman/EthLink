d30ef97063960a33531de5b75c819a1ddd5ac038
Prajwal Benedict Norman <normanprajwal@gmail.com>
2025-07-20 15:58:39 +0530

    update ui for signin page

--- Frontend/src/Pages/Signin.tsx
+++ Frontend/src/Pages/Signin.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from "react"
 import { useNavigate, useSearchParams } from "react-router-dom"
 import axios from "axios"
+import Logo from "../Components/Logo"
+import Button from "../Components/Button"
 function Signin() {
   const navigate=useNavigate()
   const [searchParam,setSearchParam]=useSearchParams()
@@ -42,24 +44,61 @@ function Signin() {
 
     }
   return (
-  <div className="bg-gray-600 min-h-screen min-w-screen fixed">
-        <div className="flex mt-48 justify-center">
-        <div className="bg-white min-h-96 rounded-xl   min-w-xl">
-        SignIn
-        <div>
-        <input type="text" className="border mt-10 max-h-7 min-w-xs mx-[22%] text-center text-xl" placeholder="Username"  ref={usernameRef}/>
+  <>
+   <div className="bg-[#0f111a] md:bg-gradient-to-br md:from-[#0f111a] md:to-[#1a1c2e] min-h-screen min-w-screen fixed">
+  <div className="md:ms-12 ms-4 mt-8">
+    <Logo />
+  </div>
+
+  <div className="flex justify-center items-center mt-12 md:mt-0 min-h-[85vh]">
+    <div className="bg-[#161823] rounded-2xl p-8 border border-[#2a2d3e] shadow-xl w-full max-w-4xl min-h-[70vh] flex flex-col md:flex-row justify-between gap-8">
+      <div className="md:w-1/2 flex flex-col justify-center">
+        <h2 className="text-3xl text-white font-semibold">Welcome Back!</h2>
+        <p className="text-white text-sm mt-6 text-left">
+          Securely connect to the decentralized world with <span className="animate-pulse text-indigo-400 font-medium">EthLink</span>,<br />
+          your trusted bridge to Web3.
+        </p>
+
+        <div className="mt-8 space-y-5">
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            className="text-white px-4 py-2 rounded bg-[#272b3e7c] w-full"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="text-white px-4 py-2 rounded bg-[#272b3e7c] w-full"
+          />
         </div>
-        <div>
-        <input type="text" className="border mt-10 max-h-7 min-w-xs mx-[22%] text-center text-xl" placeholder="Password" ref={passwordRef}/>
+
+        <div className="flex justify-center">
+          <Button variant="primary" content="Sign in" className="mt-8 text-center" />
         </div>
-        <div>
-        {!navigation && <button className="border mt-10 max-h-7 min-w-xs  mx-[22%] cursor-pointer hover:bg-sky-500" onClick={signinn}>Submit</button>}  
-        {navigation && <button className="border mt-10 max-h-7 min-w-xs  mx-[22%] cursor-pointer hover:bg-sky-500" onClick={dappSignin}>Submit</button>}  
+
+        <div className="flex items-center gap-4 my-6">
+          <div className="flex-grow h-px bg-gray-600" />
+          <span className="text-gray-400 text-sm">OR</span>
+          <div className="flex-grow h-px bg-gray-600" />
         </div>
-      </div> 
+
+        <div className="flex items-center justify-center gap-12">
+          <img src="EthLa.png" alt="Ethereum icon" className="h-10 w-10 hover:-translate-y-2 duration-700" />
+          <img src="Metamask.png" alt="Metamask icon" className="h-10 w-10 hover:-translate-y-2 duration-700" />
         </div>
-      
+      </div>
+
+
+      <div className="hidden md:block md:w-1/2">
+
+      </div>
     </div>
+  </div>
+</div>
+
+  </>
   )
 }
 
--- Frontend/src/Pages/Signup.tsx
+++ Frontend/src/Pages/Signup.tsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { useNavigate } from "react-router-dom"
 import { Link } from "react-router-dom"
 import Button from "../Components/Button"
+import Logo from "../Components/Logo"
 function Signup() {
     const [username,setUsername]=useState("")
     const [password,setPassword]=useState("")
@@ -15,17 +16,24 @@ function Signup() {
     } 
   return (
     <>
-  <div className="bg-[#0f111a] md:bg-gradient-to-br md:from-[#0f111a] md:to-[#1a1c2e] min-h-screen min-w-screen fixed flex items-center justify-center">
+  <div className="bg-[#0f111a] md:bg-gradient-to-br md:from-[#0f111a] md:to-[#1a1c2e] min-h-screen min-w-screen fixed ">
+    <div className="md:ms-12 ms-4 mt-8">
+      <Logo />
+    </div>
+    <div className="flex mt-12 justify-center ">
     <div className="bg-[#161823] rounded-2xl p-8 border border-[#2a2d3e] shadow-xl w-full max-w-4xl min-h-[70vh] relative flex  justify-between ">
-      <div>
-        {/* Card */}
+      <div className="hidden md:block md:w-1/2">
+          {/* Cards */}
       </div>
       <div>
         <h2 className="text-3xl text-white font-semibold me-24">Create an account</h2>
         <div className="flex items-center mt-6"><p className="text-white text-sm ">Already have an account?</p><Link to={'/signin'} className="text-white text-sm px-2 underline hover:text-blue-400">Log in</Link></div>
         <form action="submit" className="mt-12 ">
-           <input type="text" placeholder="First Name" className="text-white px-4 py-2 rounded bg-[#272b3e7c] shadow-xl "/> <input type="text" placeholder="Last Name" className="text-white px-4 py-2 rounded bg-[#272b3e7c] ms-2"/>
-           <br />
+           <div className="flex flex-col md:flex-row md:space-x-2">
+            <input type="text" placeholder="First Name" className="text-white px-4 py-2 rounded bg-[#272b3e7c] shadow-xl w-full md:w-fit"/>
+             <br className="block sm:hidden" /> 
+            <input type="text" placeholder="Last Name" className="text-white px-4 py-2 rounded bg-[#272b3e7c]  w-full md:w-fit md:ms-2"/>
+           </div>
           <input type="text" placeholder="Username" className="text-white px-4 py-2 rounded bg-[#272b3e7c] mt-4 w-full"/>
           <br />
           <input type="password" placeholder="Enter your password" className="text-white px-4 py-2.5 rounded bg-[#272b3e7c] mt-4 w-full"/>
@@ -35,9 +43,21 @@ function Signup() {
         <input type="checkbox"  /> <p className="text-white text-sm px-2">I agree to the <Link to={'/'} className="underline ">Terms & Conditions</Link></p>
         </div>
         <div className="flex justify-center">
-        <Button content="Create account" variant="primary" className="mt-8 text-center"/>
+        <Button content="Create account" variant="primary" className="mt-6 text-center"/>
         </div>
+        <div className="flex items-center gap-4 my-4">
+          <div className="flex-grow h-px bg-gray-300"></div>
+          <span className="text-gray-500 text-sm">OR</span>
+             <div className="flex-grow h-px bg-gray-300"></div>
+        </div>
+        <div className="flex items-center justify-center">
+          <img src="EthLa.png" alt="Ethereum icon" className="h-10 w-10 hover:-translate-y-2 duration-700"/>
+          <img src="Metamask.png" alt="Metamask icon" className="h-10 w-10 ms-12  hover:-translate-y-2 duration-700"/>
+        </div>
+
       </div>
+    
+    </div>
     </div>
   </div>
   </>
