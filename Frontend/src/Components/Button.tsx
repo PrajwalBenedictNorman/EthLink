1e97703245e8d82d5bac8b21a63f966fb6c3b0a7
Prajwal Benedict Norman <normanprajwal@gmail.com>
2025-09-20 01:56:24 +0530

    started ui of dapps page

--- Frontend/src/Components/Button.tsx
+++ Frontend/src/Components/Button.tsx
@@ -1,7 +1,7 @@
 import type { ReactElement } from "react"
 import clsx from "clsx";
 
-type Variant = "primary" | "secondary" | "tertiary" | "quaternary";
+type Variant = "primary" | "secondary" | "tertiary" | "quaternary" |"pentanary";
 
 interface buttonProps {
   content:string,
@@ -16,7 +16,8 @@ const variantStyles: Record<Variant, string> = {
   primary: "border-transparent bg-[##131328] rounded-xl hover:bg-[#2a328c] text-white px-4 py-2 p-1 cursor-pointer",
   secondary: " border-gray-400 rounded-xl text-gray-700 hover:bg-gray-100 px-4 py-2 cursor-pointer",
   tertiary:"boder-transparent bg-[#3C83F6] rounded-xl text-black cursor-pointer px-4 py-2 hover:bg-[#3C83F6]/85 ",
-  quaternary :"bg-[#0B0C19]/65 rounded-xl text-white px-4 py-2 hover:bg-[#0B0C19]/45 border-1 border-white/45 hover:border-white cursor-pointer"
+  quaternary :"bg-[#0B0C19]/65 rounded-xl text-white px-4 py-2 hover:bg-[#0B0C19]/45 border-1 border-white/45 hover:border-white cursor-pointer",
+  pentanary:"bg-[#2F246B] hover:bg-[#3C83F6] hover:text-black rounded-xl px-4 text-white cursor-pointer"
 };
 
 
--- Frontend/src/Pages/Dapp.tsx
+++ Frontend/src/Pages/Dapp.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import SideBar from '../Components/SIdebar'
+import Navbar from '../Components/Navbar'
+import Button from '../Components/Button'
 
 function Dapp() {
   return (
     <div>
       <SideBar />
+      <Navbar />
+      <div className='bg-[#0B0C19]/65 h-screen w-full'>
+      <div className='ms-[23vw] mt-5'>
+        <h1 className='text-white text-4xl font-bold py-2'>DApps</h1>
+        <p className='text-white/55'>Discover and connect to decentralized applications</p>
+        <div className='flex items-center justify-start mt-6'>
+          <Button content='All' variant='pentanary' onClick={()=>{}} className='me-2'/>
+          <Button content='Defi' variant='pentanary' onClick={()=>{}} className='ms-2 me-2 text-md'/>
+          <Button content='NFT' variant='pentanary' onClick={()=>{}} className='ms-2 me-2 text-md'/>
+          <Button content='Social' variant='pentanary' onClick={()=>{}} className='ms-2 me-2 text-md'/>  
+        </div>
+        {/*  DApp Cards */}
+        <div className='grid grid-cols-3 gap-6 mt-12'>
+          <div className='bg-[#0B0C19] border-2 border-[#1A1B28] h-50 w-80 rounded-xl'>
+
+          </div>
+           <div className='bg-[#0B0C19] border-2 border-[#1A1B28] h-50 w-80 rounded-xl'>
+
+          </div>
+           <div className='bg-[#0B0C19] border-2 border-[#1A1B28] h-50 w-80 rounded-xl'>
+
+          </div>
+           <div className='bg-[#0B0C19] border-2 border-[#1A1B28] h-50 w-80 rounded-xl'>
+
+          </div>
+        </div>
+      </div>
+      </div>
     </div>
   )
 }
