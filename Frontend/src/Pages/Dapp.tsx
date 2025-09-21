8ebcc4f8368fb216abfef15d4dd0daacfec95ff0
Prajwal Benedict Norman <normanprajwal@gmail.com>
2025-09-21 19:38:31 +0530

    updated ui of swap and dapp page

--- Frontend/src/Pages/Dapp.tsx
+++ Frontend/src/Pages/Dapp.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import SideBar from '../Components/SIdebar'
 import Navbar from '../Components/Navbar'
 import Button from '../Components/Button'
-
+import { Zap } from 'lucide-react'
 function Dapp() {
   return (
     <div>
@@ -19,19 +19,28 @@ function Dapp() {
           <Button content='Social' variant='pentanary' onClick={()=>{}} className='ms-2 me-2 text-md'/>  
         </div>
         {/*  DApp Cards */}
-        <div className='grid grid-cols-3 gap-6 mt-12'>
-          <div className='bg-[#0B0C19] border-2 border-[#1A1B28] h-50 w-80 rounded-xl'>
+        <div className='grid grid-cols-3 gap-4 mt-12'>
+          <div className='bg-[#0B0C19] border-2 border-[#1A1B28] h-50 w-85 rounded-xl flex flex-col items-center justify-start'>
+           
+          </div>
+           <div className='bg-[#0B0C19] border-2 border-[#1A1B28] h-50 w-85 rounded-xl'>
 
           </div>
-           <div className='bg-[#0B0C19] border-2 border-[#1A1B28] h-50 w-80 rounded-xl'>
+           <div className='bg-[#0B0C19] border-2 border-[#1A1B28] h-50 w-85 rounded-xl'>
+ 
+          </div>
+           <div className='bg-[#0B0C19] border-2 border-[#1A1B28] h-50 w-85 rounded-xl'>
 
           </div>
-           <div className='bg-[#0B0C19] border-2 border-[#1A1B28] h-50 w-80 rounded-xl'>
-
-          </div>
-           <div className='bg-[#0B0C19] border-2 border-[#1A1B28] h-50 w-80 rounded-xl'>
-
-          </div>
+        </div>
+         <div className='bg-[#0B0C19] border-2 border-[#1A1B28] h-40 w-[75vw] rounded-xl mt-4'>
+        <div className='flex items-center justify-start mt-4'>
+          <Zap size={38} className='text-blue-400 ms-2 bg-sky-950 rounded-md p-2'/>
+          <h1 className='text-white ms-2 font-bold'>How to Connect</h1>
+        </div>
+        <div className='flex items-center justify-center'>
+          <div></div>
+        </div>
         </div>
       </div>
       </div>
--- Frontend/src/Pages/Swap.tsx
+++ Frontend/src/Pages/Swap.tsx
@@ -1,118 +1,85 @@
-import React, { useState } from "react";
-import { ArrowRight,ArrowDown } from "lucide-react";
-import SideBar from "../Components/SIdebar";
-
-type Coin = {
-  name: string;
-  image: string;
-};
-
+import Button from "../Components/Button"
+import Navbar from "../Components/Navbar"
+import SideBar from "../Components/SIdebar"
+import { ArrowUpDown,TrendingUp, Zap } from "lucide-react"
 function Swap() {
-  const coins: Coin[] = [
-    { name: "Ethereum", image: "/ethSwap.png" },
-    { name: "Bitcoin", image: "/Btc.png" },
-    { name: "Solana", image: "/swapSol.png" },
-  ];
-
-  const [selectedLeft, setSelectedLeft] = useState<Coin>(coins[0]);
-  const [selectedRight, setSelectedRight] = useState<Coin>(coins[1]);
-
   return (
-    <>
-    <SideBar />
-    <div className="flex justify-center md:py-1 py-4 px-4 mt-12 ms-42 items-center">
-      <div className="bg-[#0a0a16] rounded-2xl shadow-[0_0_30px_rgba(0,0,50,0.9)] border border-[#2c2c4a] 
-        h-auto md:h-[85vh] w-full max-w-[800px] flex flex-col items-center p-6 sm:p-8">
-        
+    <div>
+      <SideBar />
+      <Navbar />
+       <div className='bg-[#0B0C19]/65 h-screen w-full flex flex-col items-center'>
+       <h1 className="text-3xl font-bold text-white mt-10">Swap Tokens</h1>
+       <p className="text-white/55 py-2">Exchange tokens instantly with the best rates</p>
 
-        <h1 className="text-white text-2xl sm:text-3xl font-bold mb-8">Swap</h1>
-
-
-        <div className="flex flex-col sm:flex-row items-center justify-center gap-6 sm:gap-12 w-full">
-
-          <select
-            className="border border-white/40 rounded-2xl px-4 sm:px-6 py-3 text-white bg-[#1a1a2e] 
-              focus:outline-none hover:bg-[#222240] transition w-full sm:w-auto"
-            value={selectedLeft.name}
-            onChange={(e) =>
-              setSelectedLeft(coins.find((c) => c.name === e.target.value)!)
-            }
-          >
-            {coins.map((coin) => (
-              <option key={coin.name} value={coin.name} className="text-black">
-                {coin.name}
-              </option>
-            ))}
-          </select>
-
-
-          <div className="flex justify-center items-center border rounded-full h-10 w-10 sm:h-12 sm:w-12 border-white bg-[#1a1a2e]">
-            <ArrowRight className="text-white w-6 h-6 sm:w-9 sm:h-9 md:block hidden" />
-             <ArrowDown className="text-white w-10 h-10 block md:hidden " />
+       {/* Main card */}
+       <div className="bg-[#11121f] h-[62vh] w-[38vw] mt-4 rounded-xl shadow-2xl border-2 border-white/10">
+        <div className="px-10 py-6">
+          <h1 className="text-white text-2xl font-bold py-1">Swap</h1>
+          <div className="flex items-center justify-between py-1">
+            <p className="text-white/45 py-1">From</p>
+            <p className="text-white/45 py-1">Balance : 0.00 ETH</p>
+          </div>
+          <div className="">
+            <input type="text" placeholder="0.0" className="bg-[#171826] w-full h-8 rounded-md text-white px-3 "/>
+            {/* drop down */}
+          </div>
+          <div className="flex items-center justify-center h-12 mt-2">
+          <ArrowUpDown size={32} className="text-white bg-[#171826] border-2 border-white/15 p-1 rounded-full" />
+          </div>
+          <div className="flex items-center justify-between py-1">
+            <p className="text-white/45">To</p>
+            <p className="text-white/45">Balance : 0.00 BTC</p>
+          </div>
+          <div className="mt-1.5">
+            <input type="text" placeholder="0.0" className="bg-[#171826] w-full h-8 rounded-md text-white px-3"/>
+            {/* drop down */}
           </div>
 
-
-          <select
-            className="border border-white/40 rounded-2xl px-4 sm:px-6 py-3 text-white bg-[#1a1a2e] 
-              focus:outline-none hover:bg-[#222240] transition w-full sm:w-auto"
-            value={selectedRight.name}
-            onChange={(e) =>
-              setSelectedRight(coins.find((c) => c.name === e.target.value)!)
-            }
-          >
-            {coins.map((coin) => (
-              <option key={coin.name} value={coin.name} className="text-black">
-                {coin.name}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div className="border-t-2 border-dotted border-gray-500 w-full max-w-[600px] mt-10"></div>
-        <div className="w-full max-w-[600px]">
-          <div className="text-white/65 grid grid-cols-2 mt-4 text-sm sm:text-base">
-            <h1 className="text-center">Give</h1>
-            <h1 className="text-center">Take</h1>
+          {/*TODO: Appears after amount enter */}
+          <div className="bg-[#171826] h-25 w-full rounded-xl mt-6">
+            <div className="px-3 py-2">
+            <div className="flex items-center justify-between py-1">
+            <p className="text-white/45 text-sm">Exchange Rate</p>
+            <p className="text-white/85 text-sm">1 ETH = 1.854.32 USDC</p>
+            </div>
+            <div className="flex items-center justify-between py-1">
+            <p className="text-white/45 text-sm">Network Fee</p>
+            <p className="text-white/85 text-sm">~$12.50</p>
+            </div>
+            <div className="flex items-center justify-between py-1">
+            <p className="text-white/45 text-sm">Route</p>
+            <p className="text-white/85 text-sm">ETH - USDC</p>
+            </div>
+            </div>
           </div>
-          <div className="text-white/75 grid grid-cols-2 mt-4">
-            <h1 className="flex justify-center text-lg sm:text-2xl items-center px-4 sm:px-9">
-              <img src={selectedLeft.image} alt={selectedLeft.name} className="h-5 w-5 sm:h-6 sm:w-6 me-2" />
-              124
-            </h1>
-            <h1 className="flex justify-center text-lg sm:text-2xl items-center px-4 sm:px-9">
-              <img src={selectedRight.image} alt={selectedRight.name} className="h-5 w-5 sm:h-6 sm:w-6 me-2" />
-              124
-            </h1>
+          <div className="text-center flex items-center justify-center py-8">
+            <Button content="Enter Amount" variant="tertiary" onClick={()=>{}} />
           </div>
         </div>
-        <div className="border-t-2 border-dotted border-gray-500 w-full max-w-[600px] mt-10"></div>
-        <div className="mt-8 space-y-3 w-full max-w-[600px] text-sm sm:text-base">
-          <div className="flex justify-between text-white/60">
-            <span>Price Per Share</span>
-            <span>1 Stakely Ethereum</span>
-          </div>
-          <div className="flex justify-between text-white/60">
-            <span>Pool Health</span>
-            <span>90%</span>
-          </div>
-          <div className="flex justify-between text-white/60">
-            <span>Internal Price</span>
-            <span>1 Stakely Ethereum</span>
-          </div>
-          <div className="flex justify-between text-white/60">
-            <span>Transaction Cost</span>
-            <span>$4.50</span>
-          </div>
+       </div>
+
+        {/* Volume Cards */}
+        <div className="flex items-center">
+        <div className="bg-[#11121f] h-[10vh] w-[18vw] mt-4 rounded-xl shadow-2xl border-2 border-white/10 me-4">
+        <div className="flex items-center px-2 py-1">
+        <TrendingUp className="text-green-500"/>
+        <p className="text-white/75 ms-2">24h Volume</p>
         </div>
-        <div className="border-t-2 border-dotted border-gray-500 w-full max-w-[600px] mt-10"></div>
-        <button className="w-full sm:w-[70%] text-white bg-white/15 p-3 rounded-2xl mt-6 border border-white 
-          hover:bg-white hover:text-black transition">
-          Swap
-        </button>
-      </div>
+        <p className="text-white font-bold px-2 text-xl">$1.5B</p>
+        </div>
+        <div className="bg-[#11121f] h-[10vh] w-[18vw] mt-4 rounded-xl shadow-2xl border-2 border-white/10 ms-4">
+        <div className="flex items-center px-2 py-1">
+          <Zap className="text-blue-500"/>
+          <p className="text-white/75 ms-2">Avg. Time</p>
+        </div>  
+          <p className="text-white font-bold px-2 text-xl">~ 1.5s</p>
+        </div>
+        </div>
+
+
+       </div>
     </div>
-    </>
-  );
-}
+  )
+}3
 
-export default Swap;
+export default Swap
